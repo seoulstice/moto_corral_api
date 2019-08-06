c535fe934c293443dcfa1f68e2fce1e7190ff15f
Young Jung <jungyoungjoon@gmail.com>
2019-08-06 16:50:33 -0400

    Created/ran migration to add columns to Corral model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_20_161822) do
+ActiveRecord::Schema.define(version: 2019_08_06_204301) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,10 @@ ActiveRecord::Schema.define(version: 2019_07_20_161822) do
     t.decimal "long", precision: 10, scale: 6
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "city"
+    t.string "state"
+    t.integer "zip"
+    t.string "side"
   end
 
 end
