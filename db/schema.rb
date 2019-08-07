bc195ec513e216c675deb4e0549b5034109fdea4
Young Jung <jungyoungjoon@gmail.com>
2019-08-06 23:35:29 -0400

    Changed scale of latitude/longitude columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_06_210547) do
+ActiveRecord::Schema.define(version: 2019_08_07_031605) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "corrals", force: :cascade do |t|
     t.string "address"
-    t.decimal "latitude", precision: 10, scale: 6
-    t.decimal "longitude", precision: 10, scale: 6
+    t.decimal "latitude", precision: 10, scale: 8
+    t.decimal "longitude", precision: 10, scale: 8
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "city"
