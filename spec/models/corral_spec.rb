aaf1c282dc32bc3ab968166363a7900ec6dfdb9a
Young Jung <jungyoungjoon@gmail.com>
2019-08-07 00:19:17 -0400

    Removed latitude/longitude validation tests because those are created after_validation

--- spec/models/corral_spec.rb
+++ spec/models/corral_spec.rb
@@ -2,8 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Corral, type: :model do
     it {should validate_presence_of(:address)}
-    it {should validate_presence_of(:latitude)}
-    it {should validate_presence_of(:longitude)}
     it {should validate_presence_of(:city)}
     it {should validate_presence_of(:state)}
     it {should validate_presence_of(:country)}
