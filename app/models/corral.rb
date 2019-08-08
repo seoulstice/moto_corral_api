984b013425f76efde266439eb46bd6dec690967d
Young Jung <jungyoungjoon@gmail.com>
2019-08-07 21:58:00 -0400

    Added geokit_rails gem and config

--- app/models/corral.rb
+++ app/models/corral.rb
@@ -2,6 +2,8 @@ require 'geocoder'
 
 class Corral < ApplicationRecord
     geocoded_by :full_address
+    acts_as_mappable :lat_column_name => :latitude,
+                     :lng_column_name => :longitude
     after_validation :geocode
     validates_presence_of :address,
                           :city,
