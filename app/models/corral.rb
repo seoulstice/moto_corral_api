58a7af6cac40263712b4d36bd24ed91f7b520b8c
Young Jung <jungyoungjoon@gmail.com>
2019-08-08 19:52:55 -0400

    Added methods to Corral class to query database with geokit built-in methods

--- app/models/corral.rb
+++ app/models/corral.rb
@@ -16,8 +16,12 @@ class Corral < ApplicationRecord
         [address, city, state, country].compact.join(', ')
     end
 
+    def self.closest_corral(params)
+        Corral.closest(origin: [params[:origin_lat], params[:origin_long]]).first
+    end
 
-
-
+    def self.num_closest_corrals(params)
+        Corral.by_distance(origin: [params[:origin_lat], params[:origin_long]])[0...(params[:amount].to_i)]
+    end
 
 end
