73e50df5cb97048d64597305f22826512032bb77
Young Jung <jungyoungjoon@gmail.com>
2019-08-09 10:57:09 -0400

    Fixed indentation

--- app/controllers/api/v1/corrals_controller.rb
+++ app/controllers/api/v1/corrals_controller.rb
@@ -1,12 +1,11 @@
 class Api::V1::CorralsController < ApplicationController
 
-    def show
-        render json: Corral.find(params[:id])
-    end
+  def show
+    render json: Corral.find(params[:id])
+  end
 
-    def index
-        render json: Corral.all
-    end
-
+  def index
+    render json: Corral.all
+  end
 
 end
--- spec/lib/kml_parser_spec.rb
+++ spec/lib/kml_parser_spec.rb
@@ -2,10 +2,10 @@ require 'rails_helper'
 require './lib/models/kml_parser'
 
 RSpec.describe KMLParser, type: :model do
-    it 'exists' do
-        parser = KMLParser.new("https://www.google.com/maps/d/u/0/kml?mid=1Dq-upvAE94-uN4aLTgO9mKSNwbI&forcekml=1")
-        expect(parser).to be_instance_of(KMLParser)
-    end
+  it 'exists' do
+    parser = KMLParser.new("https://www.google.com/maps/d/u/0/kml?mid=1Dq-upvAE94-uN4aLTgO9mKSNwbI&forcekml=1")
+    expect(parser).to be_instance_of(KMLParser)
+  end
 
     
 end
